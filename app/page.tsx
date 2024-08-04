2d64971ce7128699155a58c281d41427df9655f8
Gourav Ghosal <slggourav@gmail.com>
2024-08-04 14:26:48 +0530

    integrated tabs

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,12 @@
-import React from "react";
-import Editor from "./Editor";
-import { Metadata } from "next";
-import jwt from "jsonwebtoken";
-import { JWTMessage } from "@/lib/utils";
-export default function page({ searchParams }: { searchParams: any }) {
-  let prev_text = "";
-  let prev_filename = "";
-  try {
-    if (searchParams.text) {
-      let decoded: any = jwt.verify(
-        searchParams.text,
-        `${process.env.JWT_SECRET}`
-      );
-      prev_text = decoded.text;
-      prev_filename = decoded.file;
-    }
-  } catch (err: any) {
-    console.log(err?.message);
-    if (JWTMessage.hasOwnProperty(err?.message))
-      prev_text = JWTMessage[err?.message as keyof typeof JWTMessage];
-    else prev_text = err?.message;
-    prev_filename = "simple-text.txt";
-  }
-
-  async function createURL(text: string, file: string) {
-    "use server";
-    try {
-      let token = jwt.sign({ text, file }, `${process.env.JWT_SECRET}`, {
-        expiresIn: "1d", // expires in 1 day. todo: configurable expiry
-      });
-      return { token };
-    } catch (err: any) {
-      if (JWTMessage.hasOwnProperty(err?.message))
-        return { error: JWTMessage[err?.message as keyof typeof JWTMessage] };
-      else return { error: err?.message };
-    }
-  }
+"use client";
+import { Suspense } from "react";
+import EditorWrapper from "./EditorWrapper";
+export default function page() {
   return (
-    <section className='relative '>
-      <Editor
-        prev_text={prev_text || ""}
-        prev_filename={prev_filename || ""}
-        share={createURL}
-      />
-    </section>
+    <Suspense fallback={<>Loading editor</>}>
+      <section className='relative h-full'>
+        <EditorWrapper />
+      </section>
+    </Suspense>
   );
 }
-export const metadata: Metadata = {
-  title: `Simple Text Editor `,
-  description: `Experience seamless text editing with our intuitive text editor. Edit, load, create, and download text files effortlessly in various formats including .txt, .csv, and .md files. Simplify your workflow with our user-friendly interface and versatile file handling capabilities. Boost productivity with our comprehensive text editing solution today. `,
-  authors: [{ name: "Gourav Ghosal" }],
-  openGraph: {
-    title: "Simple Text Editor",
-    description:
-      "Experience seamless text editing with our intuitive text editor. Edit, load, create, and download text files effortlessly in various formats including .txt, .csv, and .md files. Simplify your workflow with our user-friendly interface and versatile file handling capabilities. Boost productivity with our comprehensive text editing solution today.",
-  },
-  twitter: {
-    title: "Simple Text Editor",
-    description:
-      "Experience seamless text editing with our intuitive text editor. Edit, load, create, and download text files effortlessly in various formats including .txt, .csv, and .md files. Simplify your workflow with our user-friendly interface and versatile file handling capabilities. Boost productivity with our comprehensive text editing solution today.",
-  },
-};
